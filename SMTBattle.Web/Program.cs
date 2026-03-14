fafda335688eb7e11d32720c0847655a422fa615
Ciorbingus <floriceltaleent@gmail.com>
2026-03-14 12:10:47 +0200

    delete account + redesign

--- SMTBattle.Web/Program.cs
+++ SMTBattle.Web/Program.cs
@@ -1,8 +1,10 @@
-using SMTBattle.Web.Components; 
+using SMTBattle.Web.Components;
 using SMTBattle.Web.Data;
 using SMTBattle.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc; 
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -60,5 +62,36 @@ app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+
+//Logout endpoint
+app.MapPost("Account/Logout", async (
+    SignInManager<User> signInManager,
+    [FromForm] string returnUrl) =>
+{
+    await signInManager.SignOutAsync();
+    return Results.LocalRedirect(returnUrl ?? "/");
+});
+
+//Delete account endpoint
+app.MapPost("Account/Delete", async (
+    SignInManager<User> signInManager, 
+    UserManager<User> userManager,
+    ApplicationDbContext dbContext,
+    ClaimsPrincipal userPrincipal) =>
+{
+    var user = await userManager.GetUserAsync(userPrincipal);
+    if (user != null)
+    {
+        var profile = await dbContext.Set<UserProfile>().FindAsync(user.Id);
+        if (profile != null) dbContext.Remove(profile);
+        await dbContext.SaveChangesAsync();
+
+        await userManager.DeleteAsync(user);
+        
+        await signInManager.SignOutAsync();
+    }
+    return Results.LocalRedirect("/");
+});
 
 app.Run();
